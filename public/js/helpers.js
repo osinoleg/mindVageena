67afa7fb1f1bed3f4aebbc8f1b5b3949d9577091
Oleg Osin <oleg@apportable.com>
2014-07-06 21:32:51 -0700

    Made bar transition smoothly between each poll request.

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -17,16 +17,20 @@ function mindTimer()
 	    console.log(response.attention);
 
 	    var data = [response.attention, response.meditation];
+
+	    // update bar graph (if elements already exist)
+		d3.select(".chart")
+		  .selectAll("div")
+		    .data(data)
+		    .transition()
+		    .duration(3200)
+		    .style("width", function(d) { return d * 10 + "px"; })
+		    .text(function(d) { return d; });
+
 		d3.select(".chart")
 		  .selectAll("div")
 		    .data(data)
 		  .enter().append("div") // only does this if no div exists
-		    .style("width", function(d) { return d * 10 + "px"; })
-		    .text(function(d) { return d; });
-
-	    d3.select(".chart")
-		  .selectAll("div")
-		    .data(data)
 		    .style("width", function(d) { return d * 10 + "px"; })
 		    .text(function(d) { return d; });
 
@@ -37,5 +41,5 @@ function graphMind()
 {
 	clearInterval(timerCounter);
 	elapsedTime = 0;
-	timerCounter = setInterval(mindTimer, 1000); //1000 will run it every 1 second
+	timerCounter = setInterval(mindTimer, 3000); //1000 will run it every 1 second
 }
